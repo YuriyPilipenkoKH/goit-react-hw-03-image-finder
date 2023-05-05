0e3a95c314e6ea45a7c3e71a037d8ed58e3eb17b
Yuriy Pilipenko <yurik2061@gmail.com>
2023-05-05 10:03:32 +0300

    load

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
 import { Container } from 'components/Container/Container'
+import Button from 'components/Button/Button'
+import { iconLoader } from 'utils/icons'
 
 export class App extends Component {
   render() {
     return (
       <Container>
+        <Button>
+          {iconLoader}
+          Load More</Button>
         
       </Container>
     )
